ed0bea1de518e4e9ae9cfc56639fd8df783c625e
be-great <78013422+be-great@users.noreply.github.com>
2021-05-20 21:49:40 +0200

    Update Form.js

--- my-app/src/component/Form.js
+++ my-app/src/component/Form.js
@@ -9,11 +9,11 @@ export default class Form extends Component {
             <form action="http://localhost:3000/Form2">
               <label>
                Gmail:
-                <input type="email" name="gmail" />
+                <input type="email" name="gmail" required />
               </label>
               <label>
                password:
-                <input type="password" name="password" />
+                <input type="password" name="password" required />
               </label>
               
               <button type="submit">Submit</button>
